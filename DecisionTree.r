109cb5a6a14039b9aa31ae743913567f4791a320
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-09-27 16:21:25 +0900

    Update DecisionTree.r

--- DecisionTree.r
+++ DecisionTree.r
@@ -1,10 +1,14 @@
 
+# cf1) https://cran.r-project.org/web/packages/rpart.plot/rpart.plot.pdf
+# cf2) http://www.milbo.org/rpart-plot/prp.pdf
+
 library(rpart)
 library(rpart.plot)
 
 tree.fit <- rpart(colY ~ col1 + col2 + col3 + col4, data=data, cp=0.01)
 # tree.fit <- rpart(colY ~., data=data, cp=0.01)
 
+# nn=TRUE : Display the node numbers
 rpart.plot(tree.fit, box.palette="RdBu", shadow.col="gray", nn=TRUE)
 
 summary(tree.fit)
