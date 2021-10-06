c6f2d899401aa57a8856d834daa2076703fb9bad
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-10-06 10:03:06 +0900

    Update subset.r

--- subset.r
+++ subset.r
@@ -1,6 +1,11 @@
+# Row
+
 data_a <- data[age <= 30]
 data_b <- data[age > 30]
 
 data_a <- subset(data, age<=30)
 data_b <- subset(data, age>30)
 
+# Column
+
+data <- subset(data, base_yymm >= '201701')
