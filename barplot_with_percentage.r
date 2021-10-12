54bb0f09247599b623b010f7b59b5f217108b6af
YUNA <79252563+reasonmii@users.noreply.github.com>
2021-10-12 14:51:23 +0900

    Update and rename category_percentage.r to barplot_with_percentage.r

--- barplot_with_percentage.r
+++ barplot_with_percentage.r
@@ -5,58 +5,59 @@
 # lapply
 # 1) 인수로 받은 함수를 인수로 받은 리스트의 각각의 인덱스에 하나하나 적용
 # 2) 인수로 받은 함수를 인덱스의 요소만큼 실행
-
+# ex) 실행방법 : rbind(x[[1]]) rbind(x[[2]]) 
+ 
 # do.call
 # 1) 인수로 받은 함수를 리스트의 인덱스 전체에 적용
 # 2) 한번만 실행
+# ex) 실행방법 : rbind(x[[1]],x[[2]]) 
 # ---------------------------------------------------------------------
 
 library(dplyr)
 
 # Calculate the total number of each category
-# Category1 : sex (female, male, etc)
-# Category2 : seg (churn, change)
+# Category1 : seg (churn, change)
+# Category2 : sex (female, male, etc)
 
 cnt <- data %>%
-  group_by(sex, seg) %>%
+  group_by(seg, sex) %>%
   summarise(n=n())
-
-# Calculate the percentage of category2 in category1
-# ex) female = churn + change = 100%
 
 
 # ---------------------------------------------------------------------
-# Percentage of 'churn' customers in each category (female, male, etc)
+# Percentage of 'female' in each seg (churn, change)
+# ex) churn = female + male = 100%
 # ---------------------------------------------------------------------
 
 pcnt1 <- do.call(rbind,
-                 lapply(split(cnt, cnt$sex), function(x){x[x$seg=='churn', 'n']/sum(x$n)}))
+                 lapply(split(cnt, cnt$seg), function(x){x[x$sex=='FEMALE', 'n']/sum(x$n)}))
 
 # Change the column's name to 'pcnt'
 names(pcnt1) <- 'pcnt'
 
-# Add column called category1, 'sex'
-pcnt1$sex <- rownames(pcnt1)
+# Add column called category1, 'seg'
+pcnt1$seg <- rownames(pcnt1)
 
-# Add column called category2, 'seg'
-pcnt1$seg <- 'churn'
+# Add column called category2, 'sex'
+pcnt1$sex <- 'female'
 
 
 # ---------------------------------------------------------------------
-# Percentage of 'change' customers in each category (female, male, etc)
+# Percentage of 'male' in each seg (churn, change)
+# ex) churn = female + male = 100%
 # ---------------------------------------------------------------------
 
 pcnt2 <- do.call(rbind,
-                 lapply(split(cnt, cnt$sex), function(x){x[x$seg=='change', 'n']/sum(x$n)}))
+                 lapply(split(cnt, cnt$seg), function(x){x[x$sex=='MALE', 'n']/sum(x$n)}))
 
 # Change the column's name to 'pcnt'
 names(pcnt2) <- 'pcnt'
 
-# Add column called category1, 'sex'
-pcnt2$sex <- rownames(pcnt2)
+# Add column called category1, 'seg'
+pcnt2$seg <- rownames(pcnt2)
 
-# Add column called category2, 'seg'
-pcnt2$seg <- 'change'
+# Add column called category2, 'sex'
+pcnt2$sex <- 'male'
 
 
 # ---------------------------------------------------------------------
@@ -64,4 +65,7 @@ pcnt2$seg <- 'change'
 # ---------------------------------------------------------------------
 
 mg <- rbind(pcnt1, pcnt2)
+
+
+
 
