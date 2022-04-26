2fa18d6a242aa8acd7a6b32d1263b4132024ab36
YUNA <79252563+reasonmii@users.noreply.github.com>
2022-04-26 21:18:04 +0900

    Update within.r

--- within.r
+++ within.r
@@ -4,6 +4,7 @@
 # ★ 주의 : within 함수에서는 새로운 변수 정의 시 반드시 '<-'를 사용해야 함 ('=' X)
 within(dataframe, newColumn <- col1 / col2)
 
-# 새로운 변수 여러 개 정의
-within(dataframe, {newColumn1 <- col1 / col2; newColumn2 <- col1 + col2})
+# 새로운 변수1을 생성하면서
+# 새로운 변수1을 사용한 새로운 변수2도 동시에 생성
+within(dataframe, {newColumn1 <- col1 / col2; newColumn2 <- newColumn1 + col2})
 
